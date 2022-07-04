fb0d999f46f774fa3ee5f156afcafe7fbb28fb9d
Bryan P. Reyes <bryanpr17@gmail.com>
2022-07-04 20:25:31 +0800

    Initial update

--- client/src/components/TableRow.jsx
+++ client/src/components/TableRow.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../context/mutation/clients';
 import { GET_CLIENTS } from '../context/queries/clients';
 
 const TableRow = ({ name, email, age, id }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [formData, setFormData] = useState({
+    name,
+    email,
+    age,
+    id,
+  });
+
   const [Delete_Client, { error }] = useMutation(DELETE_CLIENT, {
     variables: { id },
     update(cache, { data: { deleteClient } }) {
@@ -23,7 +31,20 @@ const TableRow = ({ name, email, age, id }) => {
 
   return (
     <tr>
-      <td>{name}</td>
+      <td>
+        {isEdit ? (
+          <input
+            className="form-control"
+            type="text"
+            value={formData.name}
+            onChange={(e) =>
+              setFormData((oldData) => ({ ...oldData, name: e.target.value }))
+            }
+          />
+        ) : (
+          name
+        )}
+      </td>
       <td>{email}</td>
       <td>{age}</td>
       <td>
@@ -31,8 +52,20 @@ const TableRow = ({ name, email, age, id }) => {
           type="button"
           className="btn btn-danger me-1"
           onClick={Delete_Client}
+          disabled={isEdit}
         >
           <i className="bi bi-trash"></i>
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary me-1"
+          onClick={() => setIsEdit((prevVal) => !prevVal)}
+        >
+          {isEdit ? (
+            <i className="bi bi-person-check"></i>
+          ) : (
+            <i className="bi bi-pencil-square"></i>
+          )}
         </button>
       </td>
     </tr>
